7092cec8e17f04e0347cc20e63f02af2b824dfbc
henri <henri@DESKTOP-DF698P3.fritz.box>
2021-11-23 17:20:21 +0100

    Update

---
+++ se1c4/src/se1c4/GUI.java
@@ -0,0 +1,13 @@
+package se1c4;
+import java.util.Arrays;
+
+public class GUI {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println(Arrays.toString(ziehung.lottoziehung()));
+		
+	}
+
+}
